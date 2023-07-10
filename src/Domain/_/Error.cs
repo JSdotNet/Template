912d9050f9b6029953ea34aa5751a63082ceec74
Job Schepers <j.schepers@innovadis.com>
2023-07-10 18:10:27 +0200

    Cleanup and package updates

--- src/Domain/_/Error.cs
+++ src/Domain/_/Error.cs
@@ -1,10 +1,8 @@
 ﻿namespace SolutionTemplate.Domain._;
 
 #pragma warning disable CA1716
-public readonly record struct Error(string Code, string Message, params object?[] Arguments)
+public readonly record struct Error(string Code, string Message)
 #pragma warning restore CA1716
 {
     public static implicit operator string(Error error) => error.Code;
-
-    //public string FormattedMessage => string.Format(Message, Arguments);
 }
