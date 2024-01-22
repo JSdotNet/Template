cd982b134673eb620d3eb6f4284134cf7291ccc9
Job Schepers <j.schepers@innovadis.com>
2024-01-22 14:07:22 +0100

    file scope namespaces

--- src/Infrastructure.EF/Migrations/20230705135547_AddOutbox.cs
+++ src/Infrastructure.EF/Migrations/20230705135547_AddOutbox.cs
@@ -3,36 +3,35 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
-namespace SolutionTemplate.Infrastructure.EF.Migrations
+namespace SolutionTemplate.Infrastructure.EF.Migrations;
+
+/// <inheritdoc />
+public partial class AddOutbox : Migration
 {
     /// <inheritdoc />
-    public partial class AddOutbox : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.CreateTable(
-                name: "OutboxMessage",
-                columns: table => new
-                {
-                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
-                    Type = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    Content = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    OccurredOnUtc = table.Column<DateTime>(type: "datetime2", nullable: false),
-                    ProcessedDateUtc = table.Column<DateTime>(type: "datetime2", nullable: true),
-                    Error = table.Column<string>(type: "nvarchar(max)", nullable: true)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_OutboxMessage", x => x.Id);
-                });
-        }
+        migrationBuilder.CreateTable(
+            name: "OutboxMessage",
+            columns: table => new
+            {
+                Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                Type = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                Content = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                OccurredOnUtc = table.Column<DateTime>(type: "datetime2", nullable: false),
+                ProcessedDateUtc = table.Column<DateTime>(type: "datetime2", nullable: true),
+                Error = table.Column<string>(type: "nvarchar(max)", nullable: true)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_OutboxMessage", x => x.Id);
+            });
+    }
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropTable(
-                name: "OutboxMessage");
-        }
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropTable(
+            name: "OutboxMessage");
     }
 }
